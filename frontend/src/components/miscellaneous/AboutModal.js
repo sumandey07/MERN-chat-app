d5ab132fb2884d0c308aeeaa40aa30de94f7d3d7
Suman <sumankumardey89@gmail.com>
2023-05-02 21:39:46 +0530

    changes

--- frontend/src/components/miscellaneous/AboutModal.js
+++ frontend/src/components/miscellaneous/AboutModal.js
@@ -9,6 +9,8 @@ import {
   IconButton,
   ModalFooter,
   Button,
+  Link,
+  Text,
 } from "@chakra-ui/react";
 import { MdInfoOutline } from "react-icons/md";
 import { ReactSVG } from "react-svg";
@@ -27,7 +29,7 @@ const AboutModal = ({ children }) => {
           onClick={onOpen}
         />
       )}
-      <Modal onClose={onClose} isOpen={isOpen} isCentered>
+      <Modal onClose={onClose} isOpen={isOpen} isCentered w="100%">
         <ModalOverlay />
         <ModalContent>
           <ModalHeader
@@ -44,13 +46,22 @@ const AboutModal = ({ children }) => {
             flexDirection="row"
             alignItems="center"
             fontSize="lg"
+            pb={10}
+            px={10}
           >
             <ReactSVG src="logo.svg" />
-            Let Us Talk is a Realtime online chat application developed by Suman
-            Kumar Dey If you like this app then star the repository at github
+            <Text p={5}>
+              Let Us Talk is a Realtime Online Chat Application developed by
+              SUMAN KUMAR DEY. If you LIKE this App then STAR the repository at
+              Github
+            </Text>
           </ModalBody>
-          <ModalFooter align="center">
-            <Button>Hello</Button>
+          <ModalFooter px={24}>
+            <Button colorScheme="whatsapp" size="md" variant="outline">
+              <Link href="https://github.com/sumandey07" isExternal>
+                Source Code Available at Github
+              </Link>
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
